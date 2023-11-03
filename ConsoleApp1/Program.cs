f9165282d6a7fba2e7d2a761392d38b03ba5c423
Yichen Zhang <yzhang183@slb.com>
2023-11-03 22:46:45 +0800

    fix: change '.' separator into "•"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
-const string toBeParsed = "Case 1 - Given a parking lot, and a car, When park the car, Then return a parking ticket.\r\n• Case 2 - Given a parking lot with a parked car, and a parking ticket, When fetch the car, \r\nThen return the parked car. \r\n• Case 3 - Given a parking lot with two parked cars, and two parking tickets, When fetch the \r\ncar twice, Then return the right car with each ticket.\r\n• Case 4 - Given a parking lot, and a wrong parking ticket, When fetch the car, Then return \r\nnothing.\r\n• Case 5 - Given a parking lot, and a used parking ticket, When fetch the car, Then return \r\nnothing.\r\n• Case 6 - Given a parking lot without any position, and a car, When park the car, Then\r\nreturn nothing";
+const string toBeParsed = "Case 1 - Given a standard parking boy, who manage two parking lots, both with available position, \r\nand a car, When park the car, Then the car will be parked to the first parking lot\r\n• Case 2 - Given a standard parking boy, who manage two parking lots, first is full and second with \r\navailable position, and a car, When park the car, Then the car will be parked to the second parking \r\nlot\r\n• Case 3 - Given a standard parking boy, who manage two parking lots, both with a parked car, and \r\ntwo parking ticket, When fetch the car twice, Then return the right car with each ticket \r\n• Case 4 - Given a standard parking boy, who manage two parking lots, and an unrecognized ticket, \r\nWhen fetch the car, Then return nothing with error message \"Unrecognized parking ticket.”\r\n• Case 5 - Given a standard parking boy, who manage two parking lots, and a used ticket, When\r\nfetch the car, Then return nothing with error message \"Unrecognized parking ticket.\"\r\n• Case 6 - Given a standard parking boy, who manage two parking lots, both without any position, \r\nand a car, When park the car, Then return nothing with error message \"No available position.\"";
 
 Console.WriteLine(CaseParser.Parse(toBeParsed));
 
@@ -11,14 +11,14 @@ public static class CaseParser
     {
         toBeParsed = CleanString(toBeParsed);
 
-        string[] lines = toBeParsed.Split(".");
+        string[] lines = toBeParsed.Split("•");
 
         return string.Join('\n', lines.Select(line => BuildTestFromCase(line)));
     }
 
     private static string CleanString(string str)
     {
-        str = Regex.Replace(str, @"[^0-9a-zA-Z .]+", " "); //replace special characters into space
+        str = Regex.Replace(str, @"[^0-9a-zA-Z •]+", " "); //replace special characters into space
         str = Regex.Replace(str.Trim(), @"\s+", " ").ToLower(); //keep only one space and all to lower
         return str;
     }
